0a8cbeb023528de4600d5abca5d7978db5178bb9
Aaron D. Valade <adv@alum.mit.edu>
2012-04-27 20:12:09 +0800

    initial import

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "General Sensing LTD"
+maintainer_email "aaron@generalsensing.com"
+license          "Apache 2.0"
+description      "Installs/Configures xsbt"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+%w{ubuntu debian}.each do |os|
+  supports os
+end
+
+%w{java}.each do |cb|
+  depends cb
+end
